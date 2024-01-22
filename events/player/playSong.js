39ff0701fcf5dfd813594c394f8dd2094a3c9401
ThinhDeKo <101809381+ThinhDeKo@users.noreply.github.com>
2024-01-22 23:54:03 +0700

    playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -14,7 +14,7 @@ module.exports = async (client, queue, song) => {
     .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
 .setImage(queue.songs[0].thumbnail)
     .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
+    .setFooter({ text: 'More info - Use /help command [DeKo Phạm]' });
      
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
