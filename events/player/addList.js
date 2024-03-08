9bec9ed3d0a3ce0007ab9a591850c22eca4fcf2f
RTX-GAMINGG <96680732+RTX-GAMINGG@users.noreply.github.com>
2024-03-08 13:51:49 +0530

    addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -1,9 +1,9 @@
 const db = require("../../mongoDB");
 module.exports = async (client, queue, playlist) => {
 
-queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length + " " + '❌'})\` ❌❌` }).catch(e => { })
+  
+  
 }
-
 /*
 
   ██████╗░████████╗██╗░░██╗           
