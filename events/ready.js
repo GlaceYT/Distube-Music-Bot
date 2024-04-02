da34b1d7bdc29376364e160b1edc9423ae30ec0d
Ninja274 <117549204+Ninja274@users.noreply.github.com>
2024-04-02 11:19:35 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.5`, 
+  name: `AYA ELSAYED`, 
   type: ActivityType.Watching }), 10000);
 client.errorLog = config.errorLog
 } else {
