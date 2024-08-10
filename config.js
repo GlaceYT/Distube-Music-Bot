08327963c599b20e7f24810486fb6ba7f3d6647b
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-08-10 17:06:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  ownerID: '1004206704994566164',
-   token: process.env.TOKEN,
-  clientId: "1270401318627836006",
-  guildId: "1106591466651922502",
+  ownerID: 'YOUR_OWNER_ID',
+  token: process.env.TOKEN,
   embedColor: '#FF0000',
   geniusToken: '0CHr9jtPjR7EKraXUVv6__8ELmEwFeUwt5vYAhk-is_fGOq-10-epdMzESkMPUnf',
   language: 'en',
@@ -12,20 +10,6 @@ module.exports = {
 // Language: German (de)
 // Language: Spanish (es)
 // Language: Arabic (ar)
-  cookies:{
-    domain: ".youtube.com",
-    expirationDate: 1757764090.235338,
-    hostOnly: false,
-    httpOnly: false,
-    name: "__Secure-1PAPISID",
-    path: "/",
-    sameSite: "unspecified",
-    secure: true,
-    session: false,
-    storeId: "0",
-    value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
-    id: 1
-},
 };
 
 
