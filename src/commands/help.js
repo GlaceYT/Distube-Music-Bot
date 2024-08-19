7c10ebea703351526d951d0c24da1204aebb9581
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-08-19 17:14:20 +0530

    Add files via upload

---
+++ src/commands/help.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Shows all available commands with their descriptions.'),
+  
+  async execute(interaction) {
+    // Path to the folder containing your command files
+    const commandsFolder = path.join(__dirname);
+    const commandFiles = fs.readdirSync(commandsFolder).filter(file => file.endsWith('.js'));
+
+    const helpEmbed = new EmbedBuilder()
+      .setTitle('Help - Available Commands')
+      .setDescription('- Developed By : GlaceYT\n- Type : Free Version')
+      .setColor('#0099ff');
+
+    commandFiles.forEach(file => {
+      const command = require(path.join(commandsFolder, file));
+      if (command.data && command.data.name && command.data.description) {
+        helpEmbed.addFields({
+          name: `/${command.data.name}`,
+          value: command.data.description,
+        });
+      }
+    });
+
+    await interaction.reply({ embeds: [helpEmbed] });
+  },
+};
