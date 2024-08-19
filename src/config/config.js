c776191f240f28d09e4c89cea0332f21196e5f37
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-08-19 17:26:34 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
-// config/config.js
+require('dotenv').config(); 
 module.exports = {
     token: '',
     clientId: 'botid',
     guildId: 'yourserverid',
     youtubeApiKey: process.env.YOUTUBE_API_KEY,
+    mongoUri: process.env.MONGO_URI,
     spotify: {
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
