7906cc744847d9e65bb52bacf8f3c63438d8016b
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2024-08-20 14:26:40 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,37 @@
-require('dotenv').config(); 
+require('dotenv').config();
+
+
 module.exports = {
     token: '',
-    clientId: 'botid',
-    guildId: 'yourserverid',
+    clientId: '1273230302822797312',
+    guildId: '1106591466651922502',
     youtubeApiKey: process.env.YOUTUBE_API_KEY,
     mongoUri: process.env.MONGO_URI,
     spotify: {
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-   },
-    distubeOptions: { 
-        emitNewSongOnly: false, 
-        savePreviousSongs: true, 
-      },
-  };
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    },
+    cookies:{
+        domain: ".youtube.com",
+        expirationDate: 1757764090.235338,
+        hostOnly: false,
+        httpOnly: false,
+        name: "__Secure-1PAPISID",
+        path: "/",
+        sameSite: "unspecified",
+        secure: true,
+        session: false,
+        storeId: "0",
+        value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
+        id: 1
+    },
+    
+    distubeOptions: {
+        emitAddListWhenCreatingQueue: true,
+        emitAddSongWhenCreatingQueue: false,
+        emitNewSongOnly: true,
+        joinNewVoiceChannel: true,
+        nsfw: true,
+        savePreviousSongs: true,
+    }
+};
